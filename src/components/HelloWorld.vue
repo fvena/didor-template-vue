3350b65e4f2b41cd732394adcca6ed86196c0dda
Francisco Vena <fvena@altracorporacion.es>
2018-05-14 09:31:53 +0200

    Añadido element

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,6 +24,14 @@
         a(href="http://vue-loader.vuejs.org/" target="_blank") vue-loader
       li
         a(href="https://github.com/vuejs/awesome-vue" target="_blank") awesome-vue
+
+    el-row
+      el-button Default
+      el-button(type="primary") Primary
+      el-button(type="success") Success
+      el-button(type="info") Info
+      el-button(type="warning") Warning
+      el-button(type="danger") Danger
 </template>
 
 <script>
